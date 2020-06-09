a87fd106c172dad562c5e55c21cb39c92eda1ccf
Scott Loomes <loomesno1@gmail.com>
2020-06-09 18:53:17 +1000

    Initial code commit

---
+++ myMLApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MyMLAppML.Model;
+
+namespace myMLApp
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            // Add input data
+            var input = new ModelInput {SentimentText = "That is rude."};
+
+            // Load model and predict output of sample data
+            var result = ConsumeModel.Predict(input);
+            Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}");
+        }
+    }
+}
